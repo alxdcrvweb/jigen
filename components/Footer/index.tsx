38af08b8bcd30962ed7bb1d1f9382611c2c64168
alxdcrvweb <alexweb3dev0x@proton.me>
2025-06-26 15:12:24 +0300

    replace some imports to dynamic

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,61 +1,65 @@
-
 import { FC } from "react";
-import s from '../../styles/footer.module.scss'
-import SocialList from '../SocialList'
-import logo from '../../public/logo.svg'
-import Image from 'next/image'
-import cn from 'classnames'
+import s from "../../styles/footer.module.scss";
+import SocialList from "../SocialList";
+import logo from "../../public/logo.svg";
+import Image from "next/image";
+import cn from "classnames";
 // import man from '../../public/footer/man.webp'
 import FooterLeftCircle from "./FooterLeftCircle";
 import FooterRightCircle from "./FooterRightCircle";
 import FooterMobileCircles from "./FooterMobileCircles";
-import Lottie from 'react-lottie-player'
-import man from '../../public/gifs/3.json'
+// import Lottie from "react-lottie-player";
+import man from "../../public/gifs/3.json";
+import dynamic from "next/dynamic";
 
-interface FooterProps { }
+const Lottie = dynamic(() => import("react-lottie-player"), {
+  ssr: false,
+});
+interface FooterProps {}
 const Footer: FC<FooterProps> = () => {
+  return (
+    <footer className={s.footer}>
+      <div className={cn(s.footer__container, "_container")}>
+        <div className={s.footer__body}>
+          <div className={s.footer__logo}>
+            <Image src={logo} alt={"logo"} layout="responsive" />
+          </div>
+          <div className={cn(s.footer__title, "_title", "_title_gradient")}>
+            JOIN OUR <br />
+            COMMUNITY<span>😋</span>
+          </div>
+          <div className={s.footer__social}>
+            <SocialList />
+          </div>
+          <div className={s.footer__box}>
+            <div className={s.footer__links}>
+              <FooterMobileCircles />
+              <FooterLeftCircle />
+              <div className={s.footer__man}>
+                <Lottie
+                  // className={s.utilities__image}
+                  loop
+                  animationData={man}
+                  play
+                />
+                {/* <Image src={man} alt={'man image'} layout='responsive' /> */}
+              </div>
+              <FooterRightCircle />
+            </div>
+            <div className={s.footer__bottom}>
+              <a href="#" target={"_blank"} rel="noreferrer">
+                Privacy Policy
+              </a>
+              <span></span>
+              <a href="#" target={"_blank"} rel="noreferrer">
+                Terms of Service
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
 
-	return (
-		<footer className={s.footer}>
-			<div className={cn(s.footer__container, '_container')}>
-				<div className={s.footer__body}>
-					<div className={s.footer__logo}>
-						<Image src={logo} alt={'logo'} layout='responsive' />
-					</div>
-					<div className={cn(s.footer__title, '_title', '_title_gradient')}>
-						JOIN OUR  <br />
-						COMMUNITY<span	>😋</span>
-					</div>
-					<div className={s.footer__social}>
-						<SocialList />
-					</div>
-					<div className={s.footer__box}>
-						<div className={s.footer__links}>
-							<FooterMobileCircles />
-							<FooterLeftCircle />
-							<div className={s.footer__man}>
-
-								<Lottie
-									// className={s.utilities__image}
-									loop
-									animationData={man}
-									play
-								/>
-								{/* <Image src={man} alt={'man image'} layout='responsive' /> */}
-							</div>
-							<FooterRightCircle />
-						</div>
-						<div className={s.footer__bottom}>
-							<a href="#" target={'_blank'} rel='noreferrer'>Privacy Policy</a>
-							<span></span>
-							<a href="#" target={'_blank'} rel='noreferrer'>Terms of Service</a>
-						</div>
-					</div>
-				</div>
-			</div>
-		</footer>
-	)
-}
-
-
-export default Footer
+export default Footer;
--- components/Utilities/UtilitiesItem.tsx
+++ components/Utilities/UtilitiesItem.tsx
@@ -1,38 +1,43 @@
-import { observer } from 'mobx-react'
-import Image, { StaticImageData } from 'next/image'
-import { FC } from 'react'
-import s from '../../styles/main.module.scss'
-import cn from 'classnames'
-import Lottie from 'react-lottie-player'
+import { observer } from "mobx-react";
+import Image, { StaticImageData } from "next/image";
+import { FC } from "react";
+import s from "../../styles/main.module.scss";
+import cn from "classnames";
+import dynamic from "next/dynamic";
 
+const Lottie = dynamic(() => import("react-lottie-player"), {
+  ssr: false,
+});
 
 interface UtilitiesItemProps {
-    title: string;
-    Text: () => JSX.Element;
-    icon: StaticImageData;
-    image?: StaticImageData | any;
+  title: string;
+  Text: () => JSX.Element;
+  icon: StaticImageData;
+  image?: StaticImageData | any;
 }
 
-
-const UtilitiesItem: FC<UtilitiesItemProps> = observer(({Text, icon, title, image,}) => {
-
+const UtilitiesItem: FC<UtilitiesItemProps> = observer(
+  ({ Text, icon, title, image }) => {
     return (
-        <div className={s.utilities__cell}>
-            <div className={s.utilities__icon}>
-                <Image src={icon} alt={`icon ${title}`} width={60} height={60}/>
-            </div>
-            <div className={cn(s.utilities__subtitle)}>{title}</div>
-            <div className={cn(s.utilities__text, '_text')}><Text/></div>
-            {image &&
-                <Lottie
-                    className={s.utilities__image}
-                    loop
-                    animationData={image}
-                    play
-                />
-            }
+      <div className={s.utilities__cell}>
+        <div className={s.utilities__icon}>
+          <Image src={icon} alt={`icon ${title}`} width={60} height={60} />
         </div>
-    )
-})
+        <div className={cn(s.utilities__subtitle)}>{title}</div>
+        <div className={cn(s.utilities__text, "_text")}>
+          <Text />
+        </div>
+        {image && (
+          <Lottie
+            className={s.utilities__image}
+            loop
+            animationData={image}
+            play
+          />
+        )}
+      </div>
+    );
+  }
+);
 
-export default UtilitiesItem
+export default UtilitiesItem;
